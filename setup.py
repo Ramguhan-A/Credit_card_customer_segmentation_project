7d079df15def5b189cdd48ec3652114104dc9d0a
Ramguhan-A <ramguhan99@gmail.com>
2025-04-30 12:38:55 +0530

    Exception and logger

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from setuptools import setup,find_packages
+from typing import List
 
-setup{
+def get_requirements(file_path:str) -> List[str]:
+    try:
+        with open(file_path,'r') as file_obj:
+            requirements = file_obj.read().splitlines()
     
-}
+    except FileNotFoundError:
+        print(f"Error: Requirements file not found in {file_path}")
+        return[]
+    
+    return requirements
+
+setup (
+    name = "Customer segmentation project",
+    version = "0.0.0.1,",
+    author = "Ramguhan",
+    author_email = "ramguhan99@gmail.com",
+    packages = find_packages(),
+    packages_dir = {"":"src"},
+    install_requires = get_requirements("requirements.txt"),
+    description = "A Machine Learning Project"
+)
